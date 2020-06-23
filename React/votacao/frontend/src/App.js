db489f8cb2792eb9eb9e48722aeb681e38242af7
Cacio de Castro <cacio.castro@gmail.com>
2020-06-23 00:12:31 -0300

    Aula 3 concluida

--- React/votacao/frontend/src/App.js
+++ React/votacao/frontend/src/App.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Spinner from './components/Spinner';
+import Candidates from './components/Candidates';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      candidates: [],
+    };
+
+    this.interval = null;
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      fetch("http://localhost:8080/votes")
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+          this.setState({
+            candidates: json.candidates,
+          })
+        });
+    }, 1000);
+  }
+
   render() {
-    return <h1>Votação de popularidade</h1>;
+    const { candidates } = this.state;
+
+    if (candidates.length === 0) {
+      return (
+        <Spinner description="Carregando..."/>
+      )
+    }
+    return (
+      <div>
+        <Header>Votação</ Header>
+        <Candidates candidates={candidates}/>
+      </div>
+    )
   }
 }
---
+++ React/votacao/frontend/src/components/Candidates.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Candidates({ candidates }) {
+    return (
+        <div>
+            {candidates.map(({ id, name, votes }) => {
+                return (
+                    <p key={id}>{name} - {votes}</p>
+                )
+            })}
+        </div>
+    )
+}
