079e7c737efb6899067e559dc0dc1224d1e7613f
Cacio de Castro <cacio.castro@gmail.com>
2020-06-23 23:09:08 -0300

    Aula 09

--- React/votacao/frontend/src/components/Votes.js
+++ React/votacao/frontend/src/components/Votes.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import CountUp from 'react-countup';
 
-export default function Votes({ children }) {
+export default function Votes({value, previous}) {
     return (
         <div>
-            { children }
+            <CountUp
+                start={previous}
+                end={value}
+                duration={0.1}
+                separator="."
+            >
+                {({ countUpRef }) => (
+                    <div>
+                        <span ref={countUpRef} />
+                    </div>
+                )}
+            </CountUp>
         </div>
     )
 }
